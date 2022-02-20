e6b9dde26d6873498763d21ebeb741790aff4ba3
ariana-kdr <67506710+ariana-kdr@users.noreply.github.com>
2022-02-20 18:39:04 +0100

    Accounting for ethanol in model
    - Ethanol has been added to the fuels
    - Gasoline molecular structure has been changes from iso-octane to the moles given in the NASA tables 
    - Arrays such as p_ideal etc have been changes from empty arrays to zero arrays of the appropriate size to speed up run time

--- Main_Script.m
+++ Main_Script.m
@@ -1,23 +1,22 @@
 %% General
 
-%Known Issues:
+% Known Issues:
 % - Indexing in for loop is not perfect yet
-% - For loop expans each state property matrix increasing run time
 % - Since volume is a function of time ignition and valve exhaust are not
 % instantaneous
+% - Q_lhv behaves strangely when ethanol is added to the blend, and in
+% general it has to be reconsidered due to the signs in the code not
+% matching the signs in the slides 
 
 clear all;
 close all;
 clc;
 
-%Add different path folders
-relativepath_to_generalfolder = 'General';
-addpath(relativepath_to_generalfolder);
-relativepath_to_nasafolder = 'Nasa';
-addpath(relativepath_to_nasafolder);
+% Add directory of Nasa routines to Matlab-path
+addpath('General/Nasa');    
 
-%Load Nasa Database
-TdataBase = fullfile('Nasa','NasaThermalDatabase.mat');
+% Load Nasa Database
+TdataBase = fullfile('General\Nasa','NasaThermalDatabase.mat');
 load(TdataBase);
 
 % Constants 
@@ -26,14 +25,28 @@ Runiv = 8.314472;                                                           %Uni
 Pref = 1.01235e5;                                                           %Reference Pressure (1atm) [Pa]
 Tref = 298.15;                                                              %Reference Temperature [K]
 
-%Starting with only Gasoline
-Species = Sp(myfind({Sp.Name},{'Gasoline','O2','CO2','H2O','N2'}));         %Subselection of species in database
-Mi = [Species.Mass];                                                        %Array of molecular masses for each component
-n_Nitr = (25*0.79)/(0.21);                                                  %No. moles nitrogen using air composition (79%)
+% Implementing a mixture of fuels 
+Fuel1 = 'Gasoline';                                                          % Make fuel its own variable for easy changes
+Fuel2 = 'C2H5OH'; 
+Species = Sp(myfind({Sp.Name},{Fuel1, Fuel2,'O2','CO2','H2O','N2'}));               % Subselection of species in database
+NSpecies = length(Species);                                                 % Create variable for length of Species array for later use 
+Mi = [Species.Mass];                                                        % Array of molecular masses for each component
 
-%Mole Fractions using balanced chemical equation
-X_reac = [2/(2+25+n_Nitr) 25/(2+25+n_Nitr) 0 0 n_Nitr/(2+25+n_Nitr)];
-X_prod = [0 0 16/(16+18+n_Nitr) 18/(16+18+n_Nitr) n_Nitr/(16+18+n_Nitr)];
+e = 0;                                                                      % ethanol fraction in gasoline; can vary between 0 and 1.0
+
+x = (1 - e)*Species(1).Elcomp(3) + e*Species(2).Elcomp(3);                  % Summing the moles of given ethanol/gasoline ratio for carbon
+y = (1 - e)*Species(1).Elcomp(2) + e*Species(2).Elcomp(2);                  % Summing the moles of given ethanol/gasoline ratio for hydrogen
+z = (1 - e)*Species(1).Elcomp(1) + e*Species(2).Elcomp(1);                  % Summing the moles of given ethanol/gasoline ratio for oxygen
+    
+a = x + y/4 - z/2;
+
+% Number of moles in chemical reaction
+N_reac = [1-e e a 0 0 a*3.76];
+N_prod = [0 0 0 x y/2 a*3.76];
+
+% Mole Fractions using balanced chemical equation
+X_reac = N_reac/sum(N_reac);
+X_prod = N_prod/sum(N_prod);
 
 %Convert to mass fractions
 Y_reac = (X_reac.*Mi)/(X_reac*Mi');
@@ -66,11 +79,11 @@ V_c = V_d/(r_c-1);                                                          %Cle
 % - _Isothermal/Isobaric_ exhaust/intake stroke
 
 %Create empty matrices for each state property
-p_ideal = [];                                                               %Ideal cycle pressure array [Pa]                                                       
-V_ideal = [];                                                               %Ideal cycle volume array [m^3]
-T_ideal = [];                                                               %Ideal cycle temperature array [K]
-m_ideal = [];                                                               %Ideal cycle mass array [kg]
-theta_crank = [];                                                           %Ideal cycle crank angle array [rad]
+p_ideal = zeros(1,401);                                                               %Ideal cycle pressure array [Pa]                                                       
+V_ideal = zeros(1,401);                                                               %Ideal cycle volume array [m^3]
+T_ideal = zeros(1,401);                                                               %Ideal cycle temperature array [K]
+m_ideal = zeros(1,401);                                                               %Ideal cycle mass array [kg]
+theta_crank = zeros(1,401);                                                           %Ideal cycle crank angle array [rad]
 
 %Time Values
 dt = 0.0001;                                                                %Time step to solve differential equations [s]
@@ -162,15 +175,18 @@ for j = [1:length(Species)]
 end
 Cv_2 = Y_reac*Cv_2i';                                                       %Volumetric Heat Capacity of Mixture [J/KgK]
 
-%INDICES WILL HAVE TO BE CHANGED ONCE ETHANOL ADDED 
-Q_lhv = (Y_reac)/(Y_reac(1))*h_0'-Y_prod/(Y_reac(1))*h_0';                  %Lower heating value of gasoline [J/kg] 
-                                                                            %Found to be 4.3416e6 [J/kg] online
+% Needs fixing for addition of ethanol
+% Q_lhv = Y_reac/(Y_reac(1))*h_0' - Y_prod/(Y_reac(1))*h_0' + ...
+%         Y_reac/(Y_reac(2))*h_0' - Y_prod/(Y_reac(2))*h_0';                  %Lower heating value of gasoline + ethanol[J/kg] 
+%                                                                             %Found to be 4.3416e6 [J/kg] online for gasoline
+Q_lhv = Y_reac/(Y_reac(1))*h_0' - Y_prod/(Y_reac(1))*h_0';
+
                                                                             
 V3 = V2;                                                                    %Isochoric
 T3 = T2+(Q_lhv*m2*Y_reac(1))/(m2*Cv_2);                                     %Assume instant heating up 
 m3 = m2;                                                                    
 
-p3 = (m3*R_prod*T3)/(V3);                                                   %Ideal Gas Law
+p3 = (m3*R_reac*T3)/(V3);                                                   %Ideal Gas Law
 
 i = i+1;                                                                    %Move forward one dt, so not instantaneous
 
@@ -225,217 +241,28 @@ title('Idealized Cycle')
 xlabel('Volume [m^3]')
 ylabel('Pressure [bar]')
 xlim([0 inf])
-ylim([0 150])
+ylim([0 inf])
 grid on
 
 plot(V_ideal,p_ideal.*1e-5)
 
 hold off
 
-%% Real Cycle 
-%Currently implemented:
-% - Heat loss during: compression, ignition and expansion
-% - Finite rate of combustion
-% - Valve timing for intake
-
-%Remaining:
-% - Heat loss in all other cycles
-% - Friction
-% - Pumping/Negative Cycle
-% - Valve timing for exhaust
-
-%Create empty matrices for each state property
-p_real = [];                                                                %Real cycle pressure array [Pa]                                                       
-V_real = V_ideal;                                                           %Real cycle volume array [m^3]
-T_real = [];                                                                %Real cycle temperature array [K]
-m_real = [];                                                                %Real cycle mass array [kg]
-
-%Parameters 
-method_heat_coeff = 'Honenbe';                                              %Use either Woschni or Honenberg relation
-t_w = 30e-3;                                                                %Estimate of thickness wall surrounding cylinder [m]
-k = 14.4;                                                                   %Conductive heat transfer coefficient [W/mK]
-ignition_offset = 25;                                                       %Degrees before TDC where ignition starts
-
-%Set the angle for the start position of each of the stages
-theta_intake_stroke = 0;
-theta_compression = 240;                                                    %Using measurement data
-theta_ignition = 360-ignition_offset;
-theta_expansion = 360;
-theta_valve_exhaust = 540;
-theta_exhaust_stroke = 540;
-
-%Find the index in the crank angle vector array for which it is at this
-%angle
-index_intake_stroke = max(find(theta_crank_degrees==theta_intake_stroke));
-index_compression = max(find(theta_crank_degrees<theta_compression))+1;
-index_ignition = max(find(theta_crank_degrees<theta_ignition))+1;
-index_expansion = max(find(theta_crank_degrees<theta_expansion))+1;
-index_valve_exhaust = max(find(theta_crank_degrees<theta_valve_exhaust))+1;
-index_exhaust_stroke = max(find(theta_crank_degrees<theta_exhaust_stroke))+1;
-
-%Find the time instant for each start of the strokes
-time_intake_stroke = time(index_intake_stroke);
-time_compression = time(index_compression);
-time_ignition = time(index_ignition);
-time_expansion = time(index_expansion);
-time_valve_exhaust = time(index_valve_exhaust);
-time_exhaust_stroke = time(index_exhaust_stroke);
-
-%% Real Intake Stroke
-%Still the same as ideal, have to add negative work
-
-i = 0;                                                                      %Index Value
-for t = [time_intake_stroke:dt:time_compression]                            %Start to end time
-    i = i+1;
-    p_real(i) = Pref;                                                       %Isobaric
-    T_real(i) = Tref;                                                       %Isothermal
-    m_real(i) = (p_real(i)*V_real(i))/(T_real(i)*R_reac);                   %Mass intake calculated using ideal gas law
-end
-%% Real Compression
-%No longer adiabatic, heat loss to surroundings is implemented
-
-for t = [time_compression+dt:dt:time_ignition]
-    i = i+1;                                                                %Keep updating same index as last time
-    
-    %Calculate the volumetric heat capacity for each molecule
-    for j = [1:length(Species)]
-        Cvj(j) = CvNasa(T_real(i-1),Species(j));
-    end
-    Cv = Y_reac*Cvj';                                                       %Calculate total heat capacity using mass fractions
-
-    m_real(i) = m_real(i-1);                                                %Mass stays constant since valves are closed
-
-    %Calculate thermal resistance depending on method selected
-    if method_heat_coeff == 'Woschni'
-        alpha = 1/(1/Woschni(B,p_real(i-1),T_real(i-1),S)+t_w/k);           %Thermal Resistance
-    else 
-        alpha = 1/(1/Honenberg(p_real(i-1),T_real(i-1),V_real(i-1),S)+t_w/k); %Total Thermal Resistance
-    end
-    
-    %Euler Forward Method:   
-    dTdt = -(alpha*Acyl(theta_crank(i-1),B,r,L))/(m_real(i)*Cv)*(T_real(i-1)-Tref)-(R_reac*T_real(i-1))/(V_real(i-1)*Cv)*dVdt(theta_crank(i),dthetadt,B,r,L);
-    T_real(i)= T_real(i-1)+dt*dTdt;
-
-    p_real(i)= (m_real(i)*R_reac*T_real(i))/(V_real(i));                    %Ideal Gas Law
-end
-%% Real Ignition
-
-%INDICES WILL HAVE TO BE CHANGED ONCE ETHANOL ADDED 
-Q_lhv = (Y_reac)/(Y_reac(1))*h_0'-Y_prod/(Y_reac(1))*h_0';                  %Lower heating value of gasoline [J/kg] 
-                                                                            %Found to be 4.3416e6 [J/kg] online
-                                                                            
-n = 3;                                                                      %Wiebe Form Factor (experimental)
-a = 5;                                                                      %Wiebe Efficiency Factor (expirimental)
-theta_s = theta_ignition*pi/180;                                            %Start angle of combustion [rad]
-theta_d = ignition_offset*pi/180;                                           %Combustion duration [rad]
-                                                                            
-for t = [time_ignition+dt:dt:time_expansion]
-    i = i+1;
-    %Calculate lower heating value
-    for j = [1:length(Species)]
-        h_0(j) = HNasa(Tref,Species(j));                                    %Calculate enthalpy of formation for each species
-        Cv_2i(j) = CvNasa(T_real(i-1),Species(j));                          %Volumetric Heat Capacity at T for each species
-    end
-    Cv_2_reac = Y_reac*Cv_2i';                                              %Volumetric Heat Capacity of Reactants [J/KgK]
-    Cv_2_prod = Y_prod*Cv_2i';                                              %Volumetric Heat Capacity of Products [J/KgK]
-    
-    %Calculate the Heat Capacity and gas constant of the mixture using the
-    %fraction of mass that has combusted (Wiebe Function)
-    Cv_2 = (1-Wiebe(n,a,theta_crank(i-1),theta_s,theta_d))*Cv_2_reac+Wiebe(n,a,theta_crank(i-1),theta_s,theta_d)*Cv_2_prod;
-    R = (1-Wiebe(n,a,theta_crank(i-1),theta_s,theta_d))*R_reac+Wiebe(n,a,theta_crank(i-1),theta_s,theta_d)*R_prod;
-    
-    m_real(i) = m_real(i-1);                                                %Mass stays constant since valves are closed
-    m_f = Y_reac(1)*m_real(i);                                              %Mass of fuel using mass composition [kg]
-    
-     %Calculate thermal resistance depending on method selected
-    if method_heat_coeff == 'Woschni'
-        alpha = 1/(1/Woschni(B,p_real(i-1),T_real(i-1),S)+t_w/k);
-    else 
-        alpha = 1/(1/Honenberg(p_real(i-1),T_real(i-1),V_real(i-1),S)+t_w/k);
-    end
-    
-    %Euler Forward Method
-    dTdt = (Q_lhv)/(m_real(i-1)*Cv_2)*dmdtheta(m_f,n,a,theta_crank(i-1),theta_s,theta_d)*dthetadt-(alpha*Acyl(theta_crank(i-1),B,r,L))/(m_real(i)*Cv)*(T_real(i-1)-Tref)-(R*T_real(i-1))/(V_real(i-1)*Cv)*dVdt(theta_crank(i),dthetadt,B,r,L);
-    T_real(i)= T_real(i-1)+dt*dTdt;
-    
-    p_real(i)= (m_real(i)*R*T_real(i))/(V_real(i));                         %Ideal Gas Law
-end
-%% Real Power Stroke
-
-for t = [time_expansion+dt:dt:time_valve_exhaust]
-    i = i+1;                                                                %Keep updating same index as last time
-    
-    %Calculate the volumetric heat capacity for each molecule
-    for j = [1:length(Species)]
-        Cvj(j) = CvNasa(T_real(i-1),Species(j));
-    end
-    Cv = Y_prod*Cvj';                                                       %Calculate total heat capacity using mass fractions
-
-    m_real(i) = m_real(i-1);                                                %Mass stays constant since valves are closed
-    
-    %Calculate thermal resistance depending on method selected
-    if method_heat_coeff == 'Woschni'
-        alpha = 1/(1/Woschni(B,p_real(i-1),T_real(i-1),S)+t_w/k);
-    else 
-        alpha = 1/(1/Honenberg(p_real(i-1),T_real(i-1),V_real(i-1),S)+t_w/k);
-    end
-
-    %Euler Forward Method:
-    dTdt = -(alpha*Acyl(theta_crank(i-1),B,r,L))/(m_real(i)*Cv)*(T_real(i-1)-Tref)-(R_prod*T_real(i-1))/(V_real(i-1)*Cv)*dVdt(theta_crank(i),dthetadt,B,r,L);
-    T_real(i)= T_real(i-1)+dt*dTdt;
-
-    p_real(i)= (m_real(i)*R_prod*T_real(i))/(V_real(i));                    %Ideal Gas Law
-end
-
-i_power = i;
-%% Real Valve Exhaust
-%Still needs to be changed for real cycle
-
-i = i+1;
-p_real(i) = Pref;
-T_real(i) = Tref;
-m_real(i) = (p_real(i)*V_real(i))/(R_prod*T_real(i));
-%% Real Exhaust Stroke
-%Still needs to be changed for real cycle
-
-for t = [time_exhaust_stroke+2*dt:dt:t_end]
-    i = i+1;
-    p_real(i) = Pref;                                                       %Isobaric
-    T_real(i) = Tref;                                                       %Isothermal
-    m_real(i) = (p_real(i)*V_real(i))/(T_real(i)*R_prod);                   %Mass intake calculated using ideal gas law
-end
-%% Figures
-
 figure 
+semilogy(V_ideal,p_ideal)
 hold on 
 set(gca,'FontSize',18) 
-title('Real Cycle')
+title('Idealized Cycle')
 xlabel('Volume [m^3]')
-ylabel('Pressure [bar]')
-xlim([0 inf])
-ylim([0 100])
-grid on
+ylabel('Pressure [Pa]')
 
-plot(V_real,p_real.*1e-5)
+xlim([0 inf])
+ylim([0 inf])
+
+grid on 
 
 hold off
 
-figure 
-hold on 
-set(gca,'FontSize',18) 
-title('Comparison')
-xlabel('Volume [m^3]')
-ylabel('Pressure [bar]')
-xlim([0 inf])
-ylim([0 150])
-grid on
-
-plot(V_ideal,p_ideal.*1e-5)
-plot(V_real,p_real.*1e-5)
-
-legend('Ideal Cycle', 'Real Cycle')
-
-hold off
 %% Functions
 
 function [Vcyl] = Vcyl(theta,B,r,L,V_c)
@@ -447,37 +274,7 @@ function [Vcyl] = Vcyl(theta,B,r,L,V_c)
     Vcyl = pi/4*B^2*d+V_c;                                                  %Cylinder Volume [m^3]
 end
 
-function [Acyl] = Acyl(theta,B,r,L)
-%Calculate cylinder area 
-    x = r*cos(theta)+sqrt(L^2-r^2*sin(theta)^2);                            %Distance from center of crankshaft to center of piston head [m]
-    d = L+r-x;                                                              %Distance of top of piston head to its maximum height [m]
-    
-    Acyl = pi*B*d+2*pi/4*B^2;
-end
-
 function [dVdt] = dVdt(theta,dthetadt,B,r,L)
 %Calculate derivative of volume with respect to time for given theta
     dVdt = pi/4*B^2*(dthetadt*r*sin(theta)+0.5*(dthetadt*r^2*sin(2*theta))/(sqrt(L^2-0.5*r^2*(1-cos(2*theta)))));
-end
-
-function [alpha] = Woschni(B,p,T,S)
-%Calculate heat transfer coefficient using Woschni Relation
-    v_p = 2*(3000)/(60)*S;                                                  %Average piston speed (used in head coeff relations)
-    alpha = 3.26*B^(-0.2)*p^(0.8)*T^(-0.55)*(2.28*v_p)^(0.8);
-end
-
-function [alpha] = Honenberg(p,T,V,S)
-%Calculate heat transfer coefficient using Honenberg Relation
-    v_p = 2*(3000)/(60)*S;                                                  %Average piston speed (used in head coeff relations)
-    alpha = 3.26*p^(0.8)*T^(-0.4)*V^(-0.06)*(v_p+1.4)^0.8;
-end
-
-function [x_frac] = Wiebe(n,a,theta,theta_s,theta_d)
-%Fraction of mass combusted using Wiebe Function
-    x_frac = 1-exp(-a*((theta-theta_s)/theta_d)^n);
-end
-
-function [dmdtheta] = dmdtheta(m_f,n,a,theta,theta_s,theta_d)
-%Wiebe function from project manual 
-    dmdtheta = m_f*n*a*(((theta-theta_s)/theta_d)^(n-1))/(theta_d)*exp(-a*((theta-theta_s)/theta_d)^n);
 end
