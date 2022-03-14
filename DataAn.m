c3ecfe962d81d543c83a23d267ee8847c396bd01
Niekkert <73135416+Niekkert@users.noreply.github.com>
2022-03-14 16:11:53 +0100

    Adjustment Drift Correction
    Rescale to ambient pressure.

--- DataAn.m
+++ DataAn.m
@@ -2,14 +2,14 @@ clear all;close all;clc
 %% add general to matlab path 
 addpath('General');
 %%
-DataDir='C:\Users\20192807\Desktop\Combustion\Triaining\matlab\Data\First attempt';  % The directory with the files. (Make sure you have backups!)
+DataDir='C:\Users\20202832\Downloads\Training';  % The directory with the files. (Make sure you have backups!)
 ColumnOrder={'time','Encoder','Sensor'};
 %% Constants
 rpm = 3000; % RPM
 t_r = 60/rpm; % Period
 omega = (2*pi*rpm)/60; % Angular velocity
 %% Converting and Importing the Data
-Files=dir(fullfile(DataDir,'10 eth half load.txt'));
+Files=dir(fullfile(DataDir,'0 eth no load.txt'));
 nFiles=length(Files);                                                       % dir gives a directory listing, only *.txt files in this case
 h=waitbar(0,'Converting');
 for i=1:nFiles
@@ -52,9 +52,7 @@ for iFiles=1:nFiles
     end
     P = movmean(P,sub); % Remove noise
     nCyc = (length(RevEnd - 1))/2; % Number of cycles
-    
-    
-    
+        
     % Input of amount of wanted cycles
     prompt2 = 'How many cycles do you want? [1-10] ';
     dlgtitle2 = 'Requested input';
@@ -64,21 +62,33 @@ for iFiles=1:nFiles
     for i = 1:out
         sub = nCyc-i;
     end
+
+    %Drift correction
+    P_amb = 1.012;      %Ambient pressure according to wheather report
+    P_exh = P(RevEnd);      %Exhaust pressure at RevEnd
+    T_exh = [ones(length(t(RevEnd)),1) t(RevEnd)];
+    Reg = T_exh\P_exh;      %Regression between pressure and time
+
+    P_drift = P + (P_amb - Reg(1,:)) - Reg(2,:) * t;
+
     figure()
     hold on
 for iCyc = 1:nCyc-sub
     Cycle_s = 2*iCyc; % Start of one cycle
     Cycle_e = Cycle_s+2; % End of one cycle
+
     t_c = t(RevEnd(Cycle_s):RevEnd(Cycle_e)); % Time of one cycle
     t_c = t_c - t_c(1);
     
-    P_c = P(RevEnd(Cycle_s):RevEnd(Cycle_e)); % Pressure of the specific cycle(s)
+    P_c = P_drift(RevEnd(Cycle_s):RevEnd(Cycle_e)); % Pressure of the specific cycle(s)
     omega_m = (4*pi)/max(t_c);              % Determined angular velocity for Ca
-    P_end = P(RevEnd);
-    Ca_off = 1.4476;
+    Ca_off = 1.4476;                        %Crank angle offset found according to ignition
     
     Ca = omega_m*t_c - Ca_off;                       % Crank angle
-    
+
+    [~,P_max(iCyc)] = max(P_drift(RevEnd(Cycle_s):RevEnd(Cycle_e)));
+    P_max_rel(iCyc) = P_max(iCyc)/length(P_drift(RevEnd(Cycle_s):RevEnd(Cycle_e)));
+
     V_c = V_cyl(Ca); % Volume using the function script
     plot(V_c,P_c);
     legend('Cycle 1','Cycle 2','Cycle 3','Cycle 4','Cycle 5','Cycle 6','Cycle 7','Cycle 8','Cycle 9','Cycle 10');
@@ -88,11 +98,5 @@ for iCyc = 1:nCyc-sub
     
     W_index = cumtrapz(V_c,P_c);
     W = W_index(end)*10^5;
-    
-%     P_end = P(RevEnd);
-%     T_end = t(RevEnd);
-%     rg = fitlm(T_end,P_end);
-%     anova(rg,'summary')
-%     plot(rg)
 end
 end
